933ccce5d5c213dfc4307ecab3c3df50c564047a
thiago.tgn <62727856+tgn76ok@users.noreply.github.com>
2023-01-30 23:43:50 -0300

    Initial commit

---
+++ categorias/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+# Create your models here.
+
+class Categoria(models.Model):
+    nome_cat = models.CharField(max_length=100)
+    
+    
+    def __str__(self):
+        return self.nome_cat
